a27c560ff4b5c937d9f269a646fcb35e4dcde8ca
Danil Samsonov <thrashdev@protonmail.com>
2025-07-02 17:12:38 +0600

    setup 5!

--- test/UserGallery.test.tsx
+++ test/UserGallery.test.tsx
@@ -1,14 +1,29 @@
-import { render } from "@testing-library/react";
-import { describe, test } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
 import { UserGalleryProvider } from "../src/UserGalleryContext";
 import { UserGallery } from "../src/UserGallery";
+import { UserAvatar } from "../src/UserAvatar";
+
+vi.mock("../src/UserAvatar", () => ({
+  UserAvatar: vi.fn(() => <div id="UserAvatar"></div>),
+}));
 
 describe("UserGallery", () => {
-  test("renders all fetched UserAvatars", () => {
+  test("renders all fetched UserAvatars", async () => {
     render(
       <UserGalleryProvider>
         <UserGallery />
       </UserGalleryProvider>
     );
+
+    await waitFor(() => {
+      expect(UserAvatar).toHaveBeenCalled();
+    });
+
+    // waitFor(() => {
+    //   expect(screen.findAllByText("0.1")).not.toBeNull();
+    // });
+    expect(UserAvatar).toHaveBeenCalledTimes(4);
+    // expect(UserAvatar).toHaveBeenCalledTimes(3);
   });
 });
